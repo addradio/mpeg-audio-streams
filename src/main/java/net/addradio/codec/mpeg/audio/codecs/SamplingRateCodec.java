ea89591dfee098ca6b37ef6f65590274e101d6db
Sebastian A. Weiß <Sebastian A. Wei@Precision-M3800>
2017-10-20 20:05:38 +0200

    first implementation of SampleRateCodec ready

--- src/main/java/net/addradio/codec/mpeg/audio/codecs/SamplingRateCodec.java
+++ src/main/java/net/addradio/codec/mpeg/audio/codecs/SamplingRateCodec.java
@@ -42,6 +42,54 @@ public final class SamplingRateCodec {
      *             if value could not be decoded.
      */
     public static final SamplingRate decode(MPEGAudioFrame frame, final int value) throws MPEGAudioCodecException {
+        switch (frame.getVersion()) {
+        case MPEG_1:
+            switch (value) {
+            case 0b00:
+                return SamplingRate._44100;
+            case 0b01:
+                return SamplingRate._48000;
+            case 0b10:
+                return SamplingRate._32000;
+            case 0b11:
+                return SamplingRate.reserved;
+            default:
+                break;
+            }
+            break;
+        case MPEG_2:
+            switch (value) {
+            case 0b00:
+                return SamplingRate._22050;
+            case 0b01:
+                return SamplingRate._24000;
+            case 0b10:
+                return SamplingRate._16000;
+            case 0b11:
+                return SamplingRate.reserved;
+            default:
+                break;
+            }
+            break;
+        case MPEG_2_5:
+            switch (value) {
+            case 0b00:
+                return SamplingRate._11025;
+            case 0b01:
+                return SamplingRate._12000;
+            case 0b10:
+                return SamplingRate._8000;
+            case 0b11:
+                return SamplingRate.reserved;
+            default:
+                break;
+            }
+            break;
+        case reserved:
+        default:
+            break;
+
+        }
         throw new MPEGAudioCodecException("Could not decode SamplingRate [valueToBeDecoded: " + value //$NON-NLS-1$
                 + "]."); //$NON-NLS-1$
     }
