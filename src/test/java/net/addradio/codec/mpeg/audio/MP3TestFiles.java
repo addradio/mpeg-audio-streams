cc55ae57fda7f90adab500b9283dad44283e2e2e
Sebastian A. Weiss <sebastian.weiss@nacamar.de>
2017-10-20 12:38:31 +0200

    added mp3 files for testing purposes

---
+++ src/test/java/net/addradio/codec/mpeg/audio/MP3TestFiles.java
@@ -0,0 +1,39 @@
+/**
+ * Class:    MP3TestFiles<br/>
+ * <br/>
+ * Created:  20.10.2017<br/>
+ * Filename: MP3TestFiles.java<br/>
+ * Version:  $Revision: $<br/>
+ * <br/>
+ * last modified on $Date:  $<br/>
+ *               by $Author: $<br/>
+ * <br/>
+ * @author <a href="mailto:sebastian.weiss@nacamar.de">Sebastian A. Weiss, nacamar GmbH</a>
+ * @version $Author: $ -- $Revision: $ -- $Date: $
+ * <br/>
+ * (c) Sebastian A. Weiss, nacamar GmbH 2012 - All rights reserved.
+ */
+
+package net.addradio.codec.mpeg.audio;
+
+/**
+ * MP3TestFiles
+ */
+public class MP3TestFiles {
+
+    /** {@link String} FILE_NAME_1000HZ_MP3 */
+    public static final String FILE_NAME_1000HZ_MP3 = "src/test/mp3/1000Hz.mp3"; //$NON-NLS-1$
+
+    /** {@link String} FILE_NAME_440HZ_MP3 */
+    public static final String FILE_NAME_440HZ_MP3 = "src/test/mp3/440Hz.mp3"; //$NON-NLS-1$
+
+    /** {@link String} FILE_NAME_ORGAN_MP3 */
+    public static final String FILE_NAME_ORGAN_MP3 = "src/test/mp3/organ.mp3"; //$NON-NLS-1$
+
+    /** {@link String} FILE_NAME_PIANO_MP3 */
+    public static final String FILE_NAME_PIANO_MP3 = "src/test/mp3/piano.mp3"; //$NON-NLS-1$
+
+    /** {@link String} FILE_NAME_SWEEP_MP3 */
+    public static final String FILE_NAME_SWEEP_MP3 = "src/test/mp3/sweep.mp3"; //$NON-NLS-1$
+
+}
