6de8f92b33fab356d39d13aa035599098785aeec
Sebastian A. Weiß <Sebastian A. Wei@Precision-M3800.fritz.box>
2017-10-23 20:53:35 +0200

    added PreReadingInputStream

---
+++ src/main/java/net/addradio/streams/PreReadingInputStream.java
@@ -0,0 +1,103 @@
+/**
+ * Class:    PreReadingInputStream<br/>
+ * <br/>
+ * Created:  23.10.2017<br/>
+ * Filename: PreReadingInputStream.java<br/>
+ * Version:  $Revision$<br/>
+ * <br/>
+ * last modified on $Date$<br/>
+ *               by $Author$<br/>
+ * <br/>
+ * @author <a href="mailto:sebastian.weiss@nacamar.de">Sebastian A. Weiss, nacamar GmbH</a>
+ * @version $Author$ -- $Revision$ -- $Date$
+ * <br/>
+ * (c) Sebastian A. Weiss, nacamar GmbH 2017 - All rights reserved.
+ */
+package net.addradio.streams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+/**
+ * PreReadingInputStream.
+ */
+public class PreReadingInputStream extends InputStream {
+
+    /** {@link InputStream} inner. */
+    private InputStream inner;
+
+    /** {@code byte[]} preReadByteArray. */
+    private byte[] preReadByteArray = null;
+
+    /** {@code int} preReadByteArrayPointer. */
+    private int preReadByteArrayPointer = -1;
+
+    /**
+     * PreReadingInputStream constructor.
+     * @param innerRef {@link InputStream} the stream to be encapsulated.
+     */
+    public PreReadingInputStream(final InputStream innerRef) {
+        setInner(innerRef);
+    }
+
+    /**
+     * getInner.
+     * @return {@link InputStream} the encapsulated inner input stream.
+     */
+    public InputStream getInner() {
+        return this.inner;
+    }
+
+    /**
+     * preReadFully. Pre-reads bytes from the underlying input stream and fills the given byte array. In further steps
+     * the pre-read array will be used to fulfill the request to standard InputStream method calls like
+     * {@link InputStream#read()} or {@link InputStream#read(byte[], int, int)}.
+     *
+     * @param toBeFilled {@code byte[]} the byte array to be filled.
+     * @return {@code int}
+     * @throws IOException due to IO problems.
+     */
+    public int preReadFully(final byte[] toBeFilled) throws IOException {
+        synchronized (this.inner) {
+            final int numberOfReadBytes = this.inner.read(toBeFilled, 0, toBeFilled.length);
+            if (numberOfReadBytes > 0) {
+                this.preReadByteArray = Arrays.copyOf(toBeFilled, numberOfReadBytes);
+                this.preReadByteArrayPointer = 0;
+            }
+            return numberOfReadBytes;
+        }
+    }
+
+    /**
+     * read.
+     * @see java.io.InputStream#read()
+     * @return {@code int}
+     * @throws IOException due to IO problems.
+     */
+    @Override
+    public int read() throws IOException {
+        synchronized (this.inner) {
+            if (this.preReadByteArray != null) {
+                if (this.preReadByteArrayPointer < this.preReadByteArray.length) {
+                    return this.preReadByteArray[this.preReadByteArrayPointer++];
+                }
+                this.preReadByteArray = null;
+                this.preReadByteArrayPointer = -1;
+            }
+            return this.inner.read();
+        }
+    }
+
+    /**
+     * setInner.
+     * @param innerRef {@link InputStream} the input stream to be encapsulated.
+     */
+    public void setInner(final InputStream innerRef) {
+        if (innerRef == null) {
+            throw new NullPointerException("innerRef MUST NOT be null!"); //$NON-NLS-1$
+        }
+        this.inner = innerRef;
+    }
+
+}
---
+++ src/test/java/net/addradio/streams/TestPreReadingInputStream.java
@@ -0,0 +1,66 @@
+/**
+ * Class:    TestPreReadingInputStream<br/>
+ * <br/>
+ * Created:  23.10.2017<br/>
+ * Filename: TestPreReadingInputStream.java<br/>
+ * Version:  $Revision$<br/>
+ * <br/>
+ * last modified on $Date$<br/>
+ *               by $Author$<br/>
+ * <br/>
+ * @author <a href="mailto:sebastian.weiss@nacamar.de">Sebastian A. Weiss, nacamar GmbH</a>
+ * @version $Author$ -- $Revision$ -- $Date$
+ * <br/>
+ * (c) Sebastian A. Weiss, nacamar GmbH 2017 - All rights reserved.
+ */
+package net.addradio.streams;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+/**
+ * TestPreReadingInputStream.
+ */
+public class TestPreReadingInputStream extends TestCase {
+
+    /**
+     * testComparingPreReadAndReadByteArray.
+     * @throws IOException due to IO problems.
+     */
+    @SuppressWarnings("static-method")
+    public void testComparingPreReadAndReadByteArray() throws IOException {
+        final byte[] inputBuffer = new byte[] { 0x4f, (byte) 0xae, 0x32, 0x7b, 0x65, (byte) 0x9a };
+        try (PreReadingInputStream pris = new PreReadingInputStream(new ByteArrayInputStream(inputBuffer))) {
+            final byte[] toBeFilled = new byte[4];
+            final int bytesRead = pris.preReadFully(toBeFilled);
+            assertEquals(toBeFilled.length, bytesRead);
+            for (int i = 0; i < bytesRead; i++) {
+                assertEquals(toBeFilled[i], inputBuffer[i]);
+                assertEquals(toBeFilled[i], (byte) pris.read());
+            }
+            for (int i = bytesRead; i < inputBuffer.length; i++) {
+                assertEquals(inputBuffer[i], (byte) pris.read());
+            }
+        }
+    }
+
+    /**
+     * testNullPointer.
+     * @throws IOException will never happen.
+     */
+    @SuppressWarnings("static-method")
+    public void testNullPointer() throws IOException {
+        try (PreReadingInputStream pris = new PreReadingInputStream(null)) {
+            assertTrue("Constructor did not throw NullPointerException.", false); //$NON-NLS-1$
+        } catch (final NullPointerException npe) {
+        }
+        try (PreReadingInputStream pris = new PreReadingInputStream(new ByteArrayInputStream("".getBytes()))) { //$NON-NLS-1$
+            pris.setInner(null);
+            assertTrue("Setter did not throw NullPointerException.", false); //$NON-NLS-1$
+        } catch (final NullPointerException npe) {
+        }
+    }
+
+}
