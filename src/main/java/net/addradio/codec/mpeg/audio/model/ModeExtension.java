15e72b51721ee4a95c16c0d88c70857ae68d6f97
Sebastian A. Weiß <sebastian.weiss@nacamar.de>
2019-12-11 16:03:41 +0100

    added some convenience infos

--- src/main/java/net/addradio/codec/mpeg/audio/model/ModeExtension.java
+++ src/main/java/net/addradio/codec/mpeg/audio/model/ModeExtension.java
@@ -21,27 +21,59 @@ package net.addradio.codec.mpeg.audio.model;
 public enum ModeExtension {
 
     /** {@link ModeExtension} COMMENT. */
-    SubBands4To31,
+    SubBands4To31(false, false),
 
     /** {@link ModeExtension} COMMENT. */
-    SubBands8To31,
+    SubBands8To31(false, false),
 
     /** {@link ModeExtension} COMMENT. */
-    SubBands12To31,
+    SubBands12To31(false, false),
 
     /** {@link ModeExtension} COMMENT. */
-    SubBands16To31,
+    SubBands16To31(false, false),
 
     /** {@link ModeExtension} COMMENT. */
-    IntensityStereo_Off__MSStereo_Off,
+    IntensityStereo_Off__MSStereo_Off(false, false),
 
     /** {@link ModeExtension} COMMENT. */
-    IntensityStereo_On__MSStereo_Off,
+    IntensityStereo_On__MSStereo_Off(false, true),
 
     /** {@link ModeExtension} COMMENT. */
-    IntensityStereo_Off__MSStereo_On,
+    IntensityStereo_Off__MSStereo_On(true, false),
 
     /** {@link ModeExtension} COMMENT. */
-    IntensityStereo_On__MSStereo_On;
+    IntensityStereo_On__MSStereo_On(true, true);
+
+    /** {@link boolean} MSStereoOn. */
+    private final boolean MSStereoOn;
+
+    /** {@link boolean} intensityStereoOn. */
+    private final boolean intensityStereoOn;
+
+    /**
+     * ModeExtension constructor.
+     * @param isMSStereoOnVal {@code boolean}
+     * @param intensityStereoOnVal {@code boolean}
+     */
+    private ModeExtension(final boolean isMSStereoOnVal, final boolean intensityStereoOnVal) {
+        this.MSStereoOn = isMSStereoOnVal;
+        this.intensityStereoOn = intensityStereoOnVal;
+    }
+
+    /**
+     * isIntensityStereoOn.
+     * @return boolean the intensityStereoOn
+     */
+    public boolean isIntensityStereoOn() {
+        return this.intensityStereoOn;
+    }
+
+    /**
+     * isMSStereoOn.
+     * @return boolean the mSStereoOn
+     */
+    public boolean isMSStereoOn() {
+        return this.MSStereoOn;
+    }
 
 }
